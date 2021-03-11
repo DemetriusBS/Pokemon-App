e3842459f0e2e6c8689d29579bb14edf32f103e4
DemetriusBS <dbsfox@outlook.com>
2021-03-11 14:31:24 -0300

    Including footer

--- src/Components/appContent.js
+++ src/Components/appContent.js
@@ -1,12 +1,15 @@
 import React, { PropTypes } from 'react'
 import Header from './header'
 import Search from './search'
+import Footer from './footer'
 
 const AppContent = () => (
     <div className='myApp'>
         <Header />
 
         <Search />
+
+        <Footer />
     </div>
 
 )
---
+++ src/Components/footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+const Footer = () => {
+
+    return <footer className='footer'>
+        <p>&copy; Demetrius Batista dos Santos - 2021</p>
+    </footer>
+
+}
+
+Footer.propTypes = {
+
+}
+
+export default Footer
